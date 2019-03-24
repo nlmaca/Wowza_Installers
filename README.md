fb85146e749534bd3bec9f1a18510771ee3e138c
Maca <jeroen@vanmarion.nl>
2019-03-24 22:47:27 +0100

    initial setup
    initial setup. added readme and 2 scripts

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # Wowza_Installers
-linux installer scripts for Wowza including Java/CSF (Firewall)
+I got tired to run the same commands over and over again when i was testing with wowza. So i made an installer for it. 
+The installer will install Java 8u202, CSF firewall including the necessary ports, It will also add the java version to Wowza.
+In this repository i might other scripts i use within wowza
+
+# references
+* Reference: https://www.wowza.com/docs/how-to-troubleshoot-wowza-streaming-engine-installation#network
+* Reference: https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-config-server-firewall-csf-on-ubuntu
+* Request Wowza trial: https://www.wowza.com/media-server/developers/license
+* Java: https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
+
+1. Ubuntu 18.04 Installer
+2. CentOS 7.6 installer
+
+# Firewall ports that will be set
+```
+Port 53: Domain name system (DNS)
+Port 80: Hypertext transfer protocol (HTTP)
+Port 113: Authentication service/identification protocol
+Port 123: Network time protocol (NTP)
+
+Port SSH_PORT: your current ssh port will be set in CSF
+OFF - 137/138/139/445 - samba ports.
+
+#Wowza Ports
+#TCP IN::
+Port 1935		: RTMP (all variants), RTSP, Microsoft Smooth Streaming, Apple HLS, MPEG-DASH, HDS, WOWZ
+Port 8084-8085	: JMX/JConsole monitoring and administration
+Port 8086-8087	: HTTP Administrator
+Port 8088		: Wowza Streaming Engine Manager
+Port 554		: RTSP
+Port 443		: SSL Connections
+Port 80		    : Licensing server connections / 
+
+UDP IN			
+Port 6790-9999  : UDP incoming streams
+```
+
+# Ubuntu Server (18.04) / CentOS server (7.6)
+The installer scripts have both been tested.
+
+# Java 8u202
+I have added the java file on my own domain. You can't download it straight from oracle anymore. You can download the file yourself and change the location if needed in the script
+search for https://vanmarion.nl/software/java/jdk-8u202-linux-x64.tar.gz
+
+# Prerequisites
+* Install a basic Ubuntu 18.04 or CentOS 7.6 server
+* Make sure you have your network set and run ssh on the port you want.
+* You need a valid developer or payed wowza license. Get it here: https://www.wowza.com/media-server/developers/license
+* When the installation of wowza starts you have to hit ENTER several times for the license agreement. 
+* you have to set a username/password for the wowza backend
+* you have to make the choice to run wowza as a service.
+
+# Wowza installer
+The script will ask for the latest download url. You can copy the Linux url from here:
+https://www.wowza.com/pricing/installer
+
+# Installation
+* Login via ssh to your server.
+* Dowwnload the installer of your choice (get the raw content)
+* run the script with sudo or root user (wowza runs as root user)
---
+++ Ubuntu1804_wowza_installer.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+# Date: 2019-03-24
+# Author: J. van Marion / jeroen@vanmarion.nl
+# Version: 1.0
+
+# Wowza installer for Ubuntu 18.04
+# Including: Java 8 Installation, Firewall CSF installation/configuration
+
+# filename: Ubuntu1804_wowza_installer.sh
+# run as: root user
+
+# ##############
+# Input data
+echo "get the latest (Linux) download version: https://www.wowza.com/pricing/installer"
+echo "example url: https://www.wowza.com/downloads/WowzaStreamingEngine-4-7-7/WowzaStreamingEngine-4.7.7-linux-x64-installer.run "
+
+read -p 'Wowza downloadlink (Linux): ' DownloadUrl
+FileName="${DownloadUrl##*/}"
+
+#update 
+clear
+echo "update your system"
+sleep 2
+
+apt-get -y update && apt-get -y upgrade
+
+#install java
+clear
+echo "install java 8u202"
+sleep 2
+cd /tmp
+wget https://vanmarion.nl/software/java/jdk-8u202-linux-x64.tar.gz
+
+tar -xzvf jdk-8u202-linux-x64.tar.gz
+rm -R /usr/lib/jvm/java-8-oracle 
+mkdir -p /usr/lib/jvm/java-8-oracle
+mv jdk1.8.0_202/* /usr/lib/jvm/java-8-oracle
+chown -R root:root /usr/lib/jvm/java-8-oracle
+
+#set java as default
+clear
+echo "set java as default"
+sleep 2
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-8-oracle/jre/bin/java 1091
+sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-8-oracle/bin/javac 1091
+
+# create file for java and add content
+clear
+echo "create file for java and add content and run the file"
+sleep 2
+echo "# add these lines to it, and save the file
+export J2SDKDIR=/usr/lib/jvm/java-8-oracle
+export J2REDIR=/usr/lib/jvm/java-8-oracle/jre
+export PATH=$PATH:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export DERBY_HOME=/usr/lib/jvm/java-8-oracle/db" >> /etc/profile.d/jdk.sh
+
+#run the file
+source /etc/profile.d/jdk.sh
+
+#check java version
+clear
+echo "the installed java version"
+java -version
+sleep 2
+
+#install Wowza
+#keep your license key ready. its needed in this installer
+clear
+echo "time to download wowza"
+sleep 2
+cd /tmp
+wget $DownloadUrl
+chmod +x $FileName
+
+#run installer
+clear
+echo "keep your license present. You need in this step. A username and password for the wowza backend needs to be set"
+echo "You have to press ENTER several times to get through the License agreement"
+echo "You also have to set a uername and password"
+echo "The installation starts in 5 seconds"
+sleep 5
+./$FileName
+
+#agree to agreement by pressing enter multiple times 	| Press [Enter] to continue:
+# accept agreement										| Do you accept this agreement? [y/n]:
+#set license key										| Please enter your Wowza Streaming Engine License Key.
+# Create Administrator Account
+# Enter a user name and password that will be used to manage Wowza StreamingEngine.
+# User Name: []: 										| name_me
+# Password: :											| xxxx
+# Confirm Password: :									| xxxx
+# Start Wowza Streaming Engine automatically [Y/n]: 	| y
+
+# Setup is now ready to begin installing Wowza Streaming Engine on your computer.
+# Do you want to continue? [Y/n]: 						| y
+
+# after wowza install set correct java version
+clear
+echo "wowza is installed. Set the java version to the 8u202"
+sleep 2
+rm -rf /usr/local/WowzaStreamingEngine/java
+ln -sf /usr/lib/jvm/java-8-oracle/ /usr/local/WowzaStreamingEngine/java
+
+#and restart everything
+clear
+echo "restart wowza services"
+sleep 1
+service WowzaStreamingEngine restart
+service WowzaStreamingEngineManager restart
+
+#install csf firewall
+clear
+echo "CSF firewall be installed and configured"
+sleep 2
+ufw disable
+
+apt-get -y install libwww-perl
+cd /tmp
+wget https://download.configserver.com/csf.tgz
+tar -xzf csf.tgz
+cd csf
+bash install.sh
+
+perl /usr/local/csf/bin/csftest.pl
+
+#firewall replace ports
+#get your current ssh port, which will be set in the firewall rules
+SSH_PORT="$(grep Port /etc/ssh/sshd_config | awk 'NR==1{print $2}')"
+echo "your current ssh port wil be set in the firewall rules:" $SSH_PORT
+
+
+sed -i 's/TESTING = "1"/TESTING = "0"/g' /etc/csf/csf.conf
+sed -i 's/TCP_IN.*/TCP_IN = "'$SSH_PORT',53,80,443,554,1935,8084:8088"/' /etc/csf/csf.conf
+sed -i 's/TCP_OUT.*/TCP_OUT = "53,80,113,443,554,1935,554"/g' /etc/csf/csf.conf
+sed -i 's/UDP_IN.*/UDP_IN = "53,6790:9999"/g' /etc/csf/csf.conf
+sed -i 's/UDP_OUT.*/UDP_OUT = "53"/g' /etc/csf/csf.conf
+
+#restart firewall
+clear
+echo "CSF firewall installed. Restart firewall services to save changes"
+sleep 2
+csf -x
+csf -e
+
+service WowzaStreamingEngine restart
+service WowzaStreamingEngineManager restart
+
+CURRENT_IP="$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')"
+clear
+echo "see below for the url to login to wowza"
+sleep 2
+
+echo "Make sure to reboot your server to check if everything is working"
+echo "Your wowza instance can be reached at: http://$CURRENT_IP:8088/enginemanager"
+
+echo "## installation is done"
